ad6f016cc485f6afedf3720189768013a89b5213
Delano Cooper <delanoscooper@gmail.com>
2014-04-19 17:24:29 -0400

    updated usage, mac only fix

--- wake.rb
+++ wake.rb
@@ -19,7 +19,7 @@ class Program
 	def run
 		if valid_arg_count?
 			if only_mac_provided?
-				WakeOnLAN.new get_mac
+				wol = WakeOnLAN.new get_mac
 				wol.wake
 			elsif mac_and_host_or_port_provided?
 				if given_flags.include? @host_flag
@@ -82,14 +82,14 @@ class Program
 	
 	def show_usage
 		print	"\nWakeOnLAN 1.0 by Delano Cooper\n\n"\
-					"Usage: send_wol_message -m <mac_address> [ -h <host_name_or_ip> ] [ -p <port_number> ]"\
+					"Usage: wake -m <mac_address> [ -h <host_name_or_ip> ] [ -p <port_number> ]"\
 					"\n\nExample 1: Wake host with mac address 11-22-33-44-55-66 on your subnet."\
-					"\n\n\tsend_wol_message -m 11-22-33-44-55-66"\
+					"\n\n\twake -m 11-22-33-44-55-66"\
 					"\n\nExample 1: Wake host with mac address 11-22-33-44-55-66 on your subnet, broadcast IP."\
-					"\n\n\tsend_wol_message -m 11-22-33-44-55-66 -h 192.168.1.255"\
+					"\n\n\twake -m 11-22-33-44-55-66 -h 192.168.1.255"\
 					"\n\nExample 2: Wake host with mac address 00:11:22:33:44:55 on a different subnet."\
-					"\n\n\tsend_wol_message -m 00:11:22:33:44:55 -h home.dyndns.org -p 30009\n"\
-					"\tsend_wol_message -m 00:11:22:33:44:55 -h 170.20.1.31 -p 51001"\
+					"\n\n\twake -m 00:11:22:33:44:55 -h home.dyndns.org -p 30009\n"\
+					"\twake -m 00:11:22:33:44:55 -h 170.20.1.31 -p 51001"\
 					"\n\nYou should forward the ports to port 9 for the target host(s).  "\
 					"NAT forward to the broadcast IP to enable sending different wake messages "\
 					"to different remote hosts.  Eg. Forward port 30009 to 192.168.1.255, port 9.  You will "\
